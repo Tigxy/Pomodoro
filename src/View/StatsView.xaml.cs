ecf647e492d25f893b7cc3042b52e950d6fd9bba
Tigxy <christian.ganhoer@outlook.com>
2021-02-16 17:57:36 +0100

    Added dark  mode_2

--- src/View/StatsView.xaml.cs
+++ src/View/StatsView.xaml.cs
@@ -11,7 +11,7 @@ namespace Pomodoro.View
         public StatsView()
         {
             InitializeComponent();
-            this.DataContext = StatsModel.Instance;
+            this.DataContext = StatisticsModel.Instance;
         }
     }
 }
